12dce8c7e22c8903a60115f78b634e774f43e90f
Dino Meyer <dinomeyer@glunz.ch>
2013-07-16 15:59:54 +0200

    remove Less and add sass

--- assets/javascripts/my.js
+++ assets/javascripts/my.js
@@ -1,1 +1,11 @@
 // My JavaScript
+jQuery(document).ready(function() {
+
+  // Replace SVG with PNG on Browser with no SVG support
+  if(!Modernizr.svg) {
+    $('img[src*="svg"]').attr('src', function() {
+      return $(this).attr('src').replace('.svg', '.png');
+    });
+  }
+
+});
